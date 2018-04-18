b70c41561357552fe5b70992aa74c6db80d81cff
James <jamescraig@finervision.com>
2018-04-19 00:49:24 +0100

    Added basic game samepl,e

--- Enijar/commands/hi.js
+++ Enijar/commands/hi.js
@@ -1,4 +1,11 @@
 var commando = require('../../commando/commando').commando;
+var bkBukkit = org.bukkit.Bukkit;
+var bkEntityType = org.bukkit.entity.EntityType;
+
+var game = {
+    wave: 1,
+    zombies: 5
+};
 
 /**
  * Usage: /hi
@@ -6,4 +13,15 @@ var commando = require('../../commando/commando').commando;
 commando('hi', function (args, player) {
     console.log('HI', args, player);
     echo(player, 'Hi ' + player.name + '!');
+
+    // Send title to player
+    bkBukkit.dispatchCommand(server.consoleSender, 'title @a title [{"text":"Wave ' + game.wave + '","color":"gold"}]');
+
+    // Spawn zombie
+
+    for (var i = 0 ; i < game.zombies * game.wave; i++) {
+        player.location.world.spawnEntity(player.location, bkEntityType.ZOMBIE);
+    }
+
+    game.wave++;
 });
