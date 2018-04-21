01be5a904e9ad56d3692db34669df79bf1f364ce
James <jamescraig@finervision.com>
2018-04-21 16:26:09 +0100

    Improved dev command

--- Enijar/commands/home.js
+++
@@ -1,8 +0,0 @@
-var commando = require('../../commando/commando').commando;
-
-/**
- * Usage: /sethome {name}
- */
-commando('sethome', function (args, player) {
-    echo(player, 'TODO: set home for player: ', player.name);
-});
